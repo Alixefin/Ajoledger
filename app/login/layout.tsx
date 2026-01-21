ccd7e382e0658102c14b0df0275629aa670dfd4d
Alixefin <efinnife@gmail.com>
2026-01-21 10:15:52 -0500

    Made some changes sha

---
+++ app/login/layout.tsx
@@ -0,0 +1,21 @@
+import { redirect } from 'next/navigation'
+import { createClient } from '@/lib/supabase/server'
+
+export default async function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const supabase = await createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  // If user is already logged in, redirect to dashboard
+  if (user) {
+    redirect('/')
+  }
+
+  return <>{children}</>
+}
